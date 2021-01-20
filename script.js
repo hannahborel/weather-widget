1b0ac26961251000b9f30cbc09bb54b5200a4933
Hannah Borel <hannahborel>
2021-01-20 12:04:28 -0600

    restructure weekday array complete. Added folders to restructure. Need to fix

--- script.js
+++ script.js
@@ -8,10 +8,17 @@
 // var sixthTemp = document.getElementById('temp-6')
 // var seventhTemp= document.getElementById('temp-7')
 
+var week = ['sun', 'mon', 'tues', 'wed', 'thur', 'fri', 'sat'];
+var newArr = [];
+dayOfWeek = 0;
+
+
+
 fetch('https://api.openweathermap.org/data/2.5/onecall?lat=29.760427&lon=-95.369804&units=imperial&appid=b016badd656d2ede8e0dbb4858e1a133')
 .then(response => response.json())
 .then(data=>{
 
+console.log(data)
 console.log(data.current.temp)
 var temp = data.current.temp
 
@@ -20,11 +27,54 @@ var featureTemp= document.getElementById("feature-temp")
 featureTemp.innerHTML = Math.ceil(temp) + '&#8457;';
 
 
+
 })
-
-
-
 .catch(err => console.log("error"))
 
-// fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid={b016badd656d2ede8e0dbb4858e1a133}')
+
+
+
+function getToday(){
+    var fullDate= new Date()
+    var dayOfWeek = fullDate.getDay()
+    console.log("Today's Date:",fullDate)
+    console.log("Day of the Week: ", dayOfWeek)
+    
+    removeIndex(dayOfWeek)
+}
+
+function removeIndex(dayOfWeek){
+
+var newArr = week.slice(dayOfWeek, week.length)
+
+var numOfElements = week.length - dayOfWeek
+week.splice(dayOfWeek, numOfElements)
+
+console.log("-------Sliced Arr--------",newArr)
+console.log("-------New Week Arr--------",week)
+
+replaceIndex(newArr)
+
+}
+
+function replaceIndex(newArr){
+
+    for (var i = newArr.length - 1; i >= 0; i--) {
+        console.log(newArr[i]);
+        week.splice(0,0, newArr[i])
+
+        console.log("-------Sliced Arr--------",newArr)
+        console.log("-------New Week Arr--------",week)
+
+    }
+
+}
+
+function printDay(week){
+
+    
+
+}
+
+getToday();
 
